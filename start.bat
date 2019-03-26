d34a1167e2a2a41792bb1caac2485fa87d9b558d
Edward Yan <eyanje@gmail.com>
2019-03-26 16:39:20 -0400

    QuizBug automation

--- start.bat
+++ start.bat
@@ -1,5 +1,3 @@
 @echo off
 
-echo Starting PHP server at 0.0.0.0:80
-
-php -S 0.0.0.0:80 -c %CD%/php.ini 
+.\startquinterest.bat | .\startquizbug.bat
---
+++ startquizbug.bat
@@ -0,0 +1,7 @@
+@echo off
+
+echo Starting QuizBug at 0.0.0.0:8080
+
+pushd Quizbug
+node .
+popd
