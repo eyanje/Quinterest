87a9db3e5640be6dd44c00d2c0f1d8cfa1a0970e
Edward Yan <eyanje@gmail.com>
2019-04-09 15:16:22 -0400

    Fix password entry

--- scripts/setupdb.bat
+++ scripts/setupdb.bat
@@ -2,7 +2,7 @@
 
 echo This script will setup the database, tables, and new user for Quinterest
 SET username=quinterestdb
-SET $password=quinterestdb
+SET password=quinterestdb
 
 echo The new user will be $username with password $password
 echo Logging into mysql as root
@@ -11,6 +11,7 @@ echo Enter the password for mysql's root if prompted
 mysql -e "GRANT ALL PRIVILEGES ON quinterestdb.* TO '%username%'@'localhost' identified by '%password%';"
 
 echo Creating databases
-mysql -u %username% --password=%password% -e "source db/createdatabase;" quinterestdb
-mysql -u %username% --password=%password% -e "source db/createtossupstable;" quinterestdb
-mysql -u %username% --password=%password% -e "source db/createbonustable;" quinterestdb
+echo When prompted for a password, enter quinterestdb
+mysql -u %username% -p -e "source db/createdatabase;" quinterestdb
+mysql -u %username% -p -e "source db/createtossupstable;" quinterestdb
+mysql -u %username% -p -e "source db/createbonustable;" quinterestdb
