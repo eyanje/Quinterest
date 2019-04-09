75e4faa460e766cc9ce751c7609fa379ccd99b0d
Edward Yan <eyanje@gmail.com>
2019-04-09 15:19:21 -0400

    Fixed password entry

--- scripts/setupdb.bat
+++ scripts/setupdb.bat
@@ -8,7 +8,7 @@ echo The new user will be $username with password $password
 echo Logging into mysql as root
 echo Enter the password for mysql's root if prompted
 
-mysql -e "GRANT ALL PRIVILEGES ON quinterestdb.* TO '%username%'@'localhost' identified by '%password%';"
+mysql -p -e "GRANT ALL PRIVILEGES ON quinterestdb.* TO '%username%'@'localhost' identified by '%password%';"
 
 echo Creating databases
 echo When prompted for a password, enter quinterestdb
